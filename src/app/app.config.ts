a456541b4b646b9a3da0eefee13b95840d3f54ba
Stuart George <stuart@accentdesign.co.uk>
2023-11-28 08:46:09 +0000

    add pwa

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,11 @@
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-import { ApplicationConfig, ErrorHandler, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig, ErrorHandler, importProvidersFrom, isDevMode } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { environment } from '@environments/environment';
 import { GlobalHttpInterceptor } from '@modules/shared/interceptors/global-http.interceptor';
 import * as Sentry from '@sentry/angular-ivy';
 import { routes } from './app.routes';
+import { provideServiceWorker } from '@angular/service-worker';
 
 if (environment.sentryDsn) {
   Sentry.init({ dsn: environment.sentryDsn });
@@ -15,15 +16,19 @@ export const appConfig: ApplicationConfig = {
     importProvidersFrom(HttpClientModule),
     provideRouter(routes, withComponentInputBinding()),
     {
-      provide: HTTP_INTERCEPTORS,
-      useClass: GlobalHttpInterceptor, multi: true,
+        provide: HTTP_INTERCEPTORS,
+        useClass: GlobalHttpInterceptor, multi: true,
     },
     (environment.sentryDsn) ? {
-      provide: ErrorHandler,
-      useValue: Sentry.createErrorHandler({ showDialog: true }),
+        provide: ErrorHandler,
+        useValue: Sentry.createErrorHandler({ showDialog: true }),
     } : {
-      provide: ErrorHandler,
-      useClass: ErrorHandler,
-    }
-  ]
+        provide: ErrorHandler,
+        useClass: ErrorHandler,
+    },
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })
+]
 };
